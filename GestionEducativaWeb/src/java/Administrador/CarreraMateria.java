cc5c8abf9d17fa393c51f133dc79842c4634ba75
Alexis Quintero <alexisquintero@hotmail.com.ar>
2018-04-17 21:18:02 -0300

    CarreraMateria puede eliminar materias

--- GestionEducativaWeb/src/java/Administrador/CarreraMateria.java
+++ GestionEducativaWeb/src/java/Administrador/CarreraMateria.java
@@ -9,8 +9,10 @@ import Entidad.Servlet;
 import Entidades.entidad;
 import Excepciones.ApplicationException;
 import Negocio.ControladorGestion;
+import Otros.Enumeraciones;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,11 +31,11 @@ import javax.servlet.http.HttpSession;
  */
 @WebServlet(name = "CarreraMateria", urlPatterns = {"/CarreraMateria"})
 public class CarreraMateria extends Servlet {
-
+    
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+    
         Entidades.Administrador usuario = null;  
         PrintWriter out = response.getWriter();  
         HttpSession session = request.getSession();
@@ -42,6 +44,10 @@ public class CarreraMateria extends Servlet {
                 (ControladorGestion)session.getAttribute("ControladorGestion");
         
         if(usuario == null) {response.sendError(401, "Login required"); return;}
+        
+        Enumeraciones.CarreraMateria redirect = 
+                Enumeraciones.CarreraMateria.
+                        valueOf(request.getParameter("redirect"));
         
         try{
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -52,27 +58,49 @@ public class CarreraMateria extends Servlet {
         List<Entidades.Materia> materias = null;
         Entidades.Carrera carrera = (Entidades.Carrera)session.getAttribute("carrera");
         List<Entidades.Materia> materiasExistentes = 
-                (List<Entidades.Materia>)(List<?>)carrera.getMaterias();        
+                (List<Entidades.Materia>)(List<?>)carrera.getMaterias();  
         
-        try {
-            materias = (List<Entidades.Materia>)
-                    (List<?>)controlador.buscarMaterias();
-        } catch (ApplicationException ex) {
-            Logger.getLogger(CarreraMateria.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        //Stream<Entidades.Carrera> materiasDisponibles = null;
-        List<Entidades.Materia> materiasDisponibles = null;
-        if(null != materias){
-            materiasDisponibles = 
-                    materias.stream().
-                    filter(m -> materiasExistentes.stream().
-                            noneMatch(me -> me.getIdMateria() == m.getIdMateria())).
-                            collect(Collectors.toList());  //TODO: Pasar a list para controlar el output          
-        }
-        
-        session.setAttribute("materiasDisponible", materiasDisponibles); 
         RequestDispatcher dispatcher = getServletContext().
-                    getRequestDispatcher("/WEB-INF/CarreraMateria.jsp");
+                            getRequestDispatcher("/WEB-INF/Error.jsp");
+        
+        switch (redirect) {
+            case Agregar: 
+                try {
+                    materias = (List<Entidades.Materia>)
+                            (List<?>)controlador.buscarMaterias();
+                } catch (ApplicationException ex) {
+                    Logger.getLogger(CarreraMateria.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                List<Entidades.Materia> materiasDisponibles = null;
+                if(null != materias){
+                    materiasDisponibles = 
+                            materias.stream().
+                            filter(m -> materiasExistentes.stream().
+                                    noneMatch(me -> me.getIdMateria() == m.getIdMateria())).
+                                    collect(Collectors.toList());         
+                }
+                
+                session.setAttribute("materiasDisponible", materiasDisponibles); 
+                dispatcher = getServletContext().
+                            getRequestDispatcher("/WEB-INF/CarreraMateria.jsp");
+                
+                break;
+            case Eliminar: 
+                int id = Integer.parseInt(request.getParameter("id"));
+                List<Entidades.Materia> materiasExistentesEliminada = 
+                        materiasExistentes.stream().filter(m -> m.getIdMateria() != id).
+                                collect(Collectors.toList());
+                ArrayList<entidad> nMaterias = new ArrayList<>(materiasExistentesEliminada);
+                carrera.setMaterias(nMaterias);
+                session.setAttribute("carrera", carrera);  
+                dispatcher = getServletContext().
+                            getRequestDispatcher("/WEB-INF/CarreraAM.jsp");
+                break;
+            default:
+                response.sendRedirect("LoginAlumno.jsp");;
+        }
+
         dispatcher.forward(request, response);
     }
 }
