c6c1f1bc07cdc7436a10de6c2cf7387e00a41bdd
Alexis Quintero <alexisquintero@hotmail.com.ar>
2018-04-19 19:16:07 -0300

    Se pueden agregar materias a las carreras
    No se pueden sacar ni se guardan en la DB

--- GestionEducativaWeb/src/java/Administrador/CarreraMateriaPost.java
+++ GestionEducativaWeb/src/java/Administrador/CarreraMateriaPost.java
@@ -6,10 +6,14 @@
 package Administrador;
 
 import Entidad.Servlet;
+import Entidades.entidad;
 import Negocio.ControladorGestion;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -37,15 +41,33 @@ public class CarreraMateriaPost extends Servlet {
         
         Entidades.Carrera carrera = (Entidades.Carrera)session.getAttribute("carrera"); 
         
-        String[] ids = request.getParameterValues("id");       
-        
+        String[] ids = request.getParameterValues("id");
         RequestDispatcher dispatcher = getServletContext().
                     getRequestDispatcher("/WEB-INF/CarreraAM.jsp"); 
-        if(ids == null){ dispatcher.forward(request, response); return; }
-       
+        if(ids == null ){ dispatcher.forward(request, response); return; }
+        
+        List<String> sids = Arrays.asList(ids);
+        List<Integer> iids = sids.stream().map(s -> Integer.valueOf(s)).
+                collect(Collectors.toList());
+        
+        for(String s: ids) iids.add(Integer.valueOf(s));
+
         ArrayList<Entidades.Materia> materiasDisponible = 
         (ArrayList<Entidades.Materia>)session.getAttribute("materiasDisponible");
         
-        //TODO: Agregar las materiasDisponibles cuyos id están en ids a la carrera en sesion
+        List<Entidades.Materia> materiasAgregar = materiasDisponible.stream().
+                filter(m -> iids.contains(m.getIdMateria())).
+                collect(Collectors.toList());
+        ArrayList<entidad> nMaterias = new ArrayList<>(materiasAgregar);  
+        
+        ArrayList<entidad> materiasExistentes = carrera.getMaterias();
+        
+        for(entidad e: nMaterias) materiasExistentes.add(e);
+        carrera.setMaterias(materiasExistentes);
+        session.setAttribute("carrera", carrera); 
+        
+        dispatcher = getServletContext().
+                    getRequestDispatcher("/WEB-INF/CarreraAM.jsp");
+        dispatcher.forward(request, response);
     }
 }
